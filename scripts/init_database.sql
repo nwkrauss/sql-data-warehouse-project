4b2bfcd4622f8c74884362b4eafe4a81114b8531
Nathan Krauss <nwkrauss@gmail.com>
2026-02-20 16:51:05 -0500

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,9 @@
+-- I'm running PostGres with PgAdmin. 
+-- Step 1:
+CREATE DATABASE DataWarehouse;
 
+-- Then I can connect to 'datawarehouse' with the Query Tool.
+-- STEP 2:
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
