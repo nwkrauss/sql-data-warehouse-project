92fb7f43290acf5eaacf530a0613117ff8e10906
Nathan Krauss <nwkrauss@gmail.com>
2026-02-20 16:58:08 -0500

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -2,9 +2,17 @@
 ============================================================
 Create Database and Schemas
 ============================================================
-Script Purpose:
-Warning:
-  */
+Script Purpose:  
+    This script creates a new database named 'DataWarehouse'.
+    Additionally, the script sets up three schemas within the database:
+    'bronze', 'silver', and 'gold'.
+
+WARNING:
+    Running this script will create a new database 'Datawarehouse'.
+    If 'Datawarehouse' already exists, all data in the database
+    might be permanently deleted. Proceed with caution and ensure
+    you have proper backups before running this.
+*/
 
 -- Step 1
 -- I'm running PostGres with PgAdmin. Start by creating a new database.
