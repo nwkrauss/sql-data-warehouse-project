e7da7347082ca9e04524e04c6d9b7a1ccf9b47c3
Nathan Krauss <nwkrauss@gmail.com>
2026-02-20 16:54:12 -0500

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,11 @@
+/*
+============================================================
+Create Database and Schemas
+============================================================
+Script Purpose:
+Warning:
+  */
+
 -- Step 1
 -- I'm running PostGres with PgAdmin. Start by creating a new database.
 CREATE DATABASE DataWarehouse;
