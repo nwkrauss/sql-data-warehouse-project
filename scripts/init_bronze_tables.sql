543c02be2e8d572df1821630cd2b5bb5e2027105
Nathan Krauss <nwkrauss@gmail.com>
2026-02-23 12:14:34 -0500

    Update init_bronze_tables.sql

--- scripts/init_bronze_tables.sql
+++ scripts/init_bronze_tables.sql
@@ -33,7 +33,7 @@ CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num VARCHAR(50),
 	sls_prd_key VARCHAR(50),
 	sls_cust_id INT,
-	sls_order_dt DATE,
+	sls_order_dt VARCHAR(50),
 	sls_ship_dt DATE,
 	sls_due_dt DATE,
 	sls_sales INT,
