8897f0929e8ae5a32899f584ee5f476c85453557
Nathan Krauss <nwkrauss@gmail.com>
2026-02-23 13:59:59 -0500

    Create DDL script for Bronze Layer tables
    This script creates tables in the 'bronze' schema, dropping existing tables if they already exist. It serves as a DDL script for the Bronze Layer.

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,11 @@
+/*
+=================================================================
+DDL Script: Create Bronze Tables
+=================================================================
+Script Purpose:  
+    This script creates tables in the 'bronze' schema, dropping
+    existing tables if they already exist.
+    Run this script to re-define the DDL of the 'bronze' tables.
+=================================================================
+*/
+
