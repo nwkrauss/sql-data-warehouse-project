3cbdde9489b04918773473525f73b8fe8010fee3
Nathan Krauss <nwkrauss@gmail.com>
2026-03-04 10:51:14 -0500

    Refactor comments in proc_load_bronze.sql
    Updated comments for consistency and formatting.

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -3,17 +3,17 @@
 Stored Procedure: Load Bronze Layer (Source -> Bronze)
 =====================================================================================
 Script Purpose:  
-    This stored procedure loads data into the 'bronze' schema from external CSV files.
-    It performs the following actions:
-      - Truncates the bronze tables before loading data.
-      - Uses the COPY command to load data from CSV files to bronze tables.
+	This stored procedure loads data into the 'bronze' schema from external CSV files.
+	It performs the following actions:
+	- Truncates the bronze tables before loading data.
+	- Uses the COPY command to load data from CSV files to bronze tables.
 
-  Parameters:
-      None.
-    This stored procedure does not accept any parameters or return any values.
+Parameters:
+	None.
+	This stored procedure does not accept any parameters or return any values.
 
-  Usage Example:
-    CALL bronze.load_bronze();
+Usage Example:
+	CALL bronze.load_bronze();
 =====================================================================================
 */
 
