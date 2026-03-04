2bcf320531b64363ef291684c1368fc499c480d3
Nathan Krauss <nwkrauss@gmail.com>
2026-03-04 10:51:51 -0500

    Fix formatting in proc_load_silver.sql
    Corrected formatting of the actions performed section.

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -5,7 +5,7 @@ Stored Procedure: Load Silver Layer (Bronze -> Silver)
 Script Purpose:  
     This stored procedure performs the ETL (Extract, Transform, Load) process to
     populate the 'silver' schema tables from the 'bronze' schema. 
-Actions Performed:
+	Actions Performed:
     - Truncates 'silver' tables.
     - Inserts transformed and cleaned data from 'bronze' into 'silver' tables.
 
