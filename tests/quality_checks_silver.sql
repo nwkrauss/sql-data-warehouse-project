da5adc9ff746e23c6d58eb1763034ec04c7ad8cc
Nathan Krauss <nwkrauss@gmail.com>
2026-03-13 15:00:10 -0400

    Add quality checks for silver schema data validation
    This script performs various quality checks for data consistency, accuracy, and standardization across the 'silver' schemas, including checks for nulls, duplicates, unwanted spaces, data standardization, and valid date ranges.

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,1 +1,50 @@
+/*
+=====================================================================================
+Quality Checks
+=====================================================================================
+Script Purpose:  
+    This script performs various quality checks for data consistency, accuracy, and 
+    standardization across the 'silver' schemas. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consitency.
+    - Invaild date ranges and orders.
+    - Data consistency between related fields.
 
+Usage Notes:
+    - Run these checks after data loading the 'silver' layer.
+    - Investigate and resolve any discrepencies found during the checks.
+=====================================================================================
+*/
+
+-- =====================================================================================
+-- Checking silver.crm_cust_info
+-- =====================================================================================
+-- Check for Nulls and Duplicates in primary key.
+-- >> Expectation: No Result
+SELECT cst_id, COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+-- Check for unwanted spaces.
+-- >> Expectation: No Result
+SELECT cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname);
+SELECT cst_lastname
+FROM silver.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname);
+
+-- Check for data standardization and consistency.
+-- >> Expectation: Only pre-defined values present ('Male', 'Female', 'n/a')
+SELECT DISTINCT(cst_marital_status)
+FROM silver.crm_cust_info;
+SELECT DISTINCT(cst_gndr)
+FROM silver.crm_cust_info;
+
+-- Verify cst_create_date column contains real dates and is correct DATE type.
+-- >> Expectation: No result.
+SELECT *
+FROM silver.crm_cust_info
+WHERE LENGTH(cst_create_date::TEXT) != 10;
