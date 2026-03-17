793b599736b56f6f4abbe2b816fe7d465c8c97b6
Nathan Krauss <nwkrauss@gmail.com>
2026-03-17 14:57:00 -0400

    Create view for customer data integration

---
+++ scripts/gold/create_view_gold.dim_customers.sql
@@ -0,0 +1,19 @@
+-- CREATING THE VIEW with a query that integrates data from 3 tables and 2 sources.
+CREATE VIEW gold.dim_customers AS
+	SELECT 
+		ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key, -- Surrogate Key
+		ci.cst_id AS customer_id,
+		ci.cst_key AS customer_number,
+		ci.cst_firstname AS first_name,
+		ci.cst_lastname AS last_name,
+		ci.cst_marital_status AS marital_status,
+		CASE
+			WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
+			ELSE COALESCE(ca.gen, 'n/a')
+		END AS gender,
+		ca.bdate AS birthdate,
+		la.cntry AS country,
+		ci.cst_create_date AS create_date
+	FROM silver.crm_cust_info ci
+		LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key = ca.cid
+		LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid;
