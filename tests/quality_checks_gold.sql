2e8febf801a1dc86c5afc8c0cc6cb11b7873ee15
Nathan Krauss <nwkrauss@gmail.com>
2026-04-05 15:11:41 -0400

    Implement data quality checks for gold layer
    Added SQL checks for data integrity in gold layer tables.

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -14,3 +14,37 @@ Usage Notes:
     - Investigate and resolve any discrepencies found during the checks.
 =====================================================================================
 */
+
+-- =====================================================================================
+-- Checking 'gold.dim_customers'
+-- =====================================================================================
+-- Check for uniqueness of Customer Key in gold.dim_customers
+-- >> Expectation: No Result
+SELECT
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+-- =====================================================================================
+-- Checking 'gold.dim_products'
+-- =====================================================================================
+-- Check for uniqueness of Product Key in gold.dim_products
+-- >> Expectation: No Result
+SELECT
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+-- =====================================================================================
+-- Checking Foreign Key Integrity in 'gold.fact_sales'
+-- =====================================================================================
+-- Check the data model connectivity between fact and dimensions.
+SELECT *
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c ON f.customer_key = c.customer_key
+LEFT JOIN gold.dim_products p ON f.product_key = p.product_key
+WHERE p.product_key IS NULL;
