d6d46c0e701fe1a2d27b9f809397f8469c8d8ed3
Nathan Krauss <nwkrauss@gmail.com>
2026-04-15 13:18:31 -0400

    Update database_exploration.sql

--- scripts/analytics/database_exploration.sql
+++ scripts/analytics/database_exploration.sql
@@ -1,1 +1,32 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
+
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
+-- Retrieve a list of all tables in the database
+SELECT *
+FROM INFORMATION_SCHEMA.TABLES
+WHERE table_schema != 'pg_catalog' AND table_schema != 'information_schema'
+ORDER BY table_schema;
+
+-- Retrieve all columns for a specific table (dim_customers)
+SELECT
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers'
+ORDER BY ordinal_position;
+
+
 
